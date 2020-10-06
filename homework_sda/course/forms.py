7e8954030d52fefdcdd7148bf86b731d52ca11c6
bvbkuba <sikora.maciej1992@gmail.com>
2020-10-07 00:43:24 +0200

    failed

--- homework_sda/course/forms.py
+++ homework_sda/course/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from course.models import Technology
 from datetime import datetime
+from django.core.exceptions import ValidationError
+from datetime import date
 
 
 def future_date(date_value):
@@ -13,14 +15,32 @@ def future_date(date_value):
 #             'Please, write correct datetime. Date of start course cannot be higher than finished date')
 
 
+class FutureDateField(forms.DateField):
+    def validate(self, value):
+        super().validate(value)
+        if value < datetime.today().date():
+            raise ValidationError('Only future dates allowed here')
+
+    def clean(self, value):
+        result = super().clean(value)
+        return date(year= result.year, month = result.month, day = 1)
+
+
+class AfterStartDateField(forms.DateField):
+    def validate(self, value):
+        super().validate(value)
+        if value <= datetime.today().date():
+            raise ValidationError('Only dates higher than start course allowed here')
+
 
 class CourseForm(forms.Form):
     title = forms.CharField(max_length=100)
     technology_id = forms.ModelChoiceField(queryset=Technology.objects)
     description = forms.CharField(widget=forms.Textarea, required=False)
-    starts = forms.DateField(validators=[future_date])
+    # starts = forms.DateField(validators=[future_date])
+    starts = FutureDateField()
     # finished = forms.DateField(validators=[end_date], required=False)
-    finished = forms.DateField(required=False)
+    finished = AfterStartDateField(required=False)
     max_atendees_count = forms.IntegerField(min_value=5, max_value=10)
     price = forms.FloatField(min_value=0.00)
     remote = forms.CheckboxInput()
